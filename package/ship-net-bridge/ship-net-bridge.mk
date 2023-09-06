fe3f27ea6e854581982f132edb9ee03447f452dc
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2023-09-06 17:47:21 +0300

    Add ship packages

---
+++ package/ship-net-bridge/ship-net-bridge.mk
@@ -0,0 +1,6 @@
+SHIP_NET_BRIDGE_VERSION = 13ec06e7690bb263179dc95da3a0879e6dbe8ea6
+SHIP_NET_BRIDGE_SITE = https://github.com/MooseTheBrown/ship-net-bridge
+SHIP_NET_BRIDGE_SITE_METHOD = git
+SHIP_NET_BRIDGE_GOMOD=github.com/moosethebrown/ship-net-bridge
+
+$(eval $(golang-package))
