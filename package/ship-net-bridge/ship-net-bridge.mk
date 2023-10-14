221d4d67f79b0159e0a1500e5b0d1b4d56a1636f
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2023-10-14 20:51:19 +0300

    Up ship-net-bridge version

--- package/ship-net-bridge/ship-net-bridge.mk
+++ package/ship-net-bridge/ship-net-bridge.mk
@@ -1,4 +1,4 @@
-SHIP_NET_BRIDGE_VERSION = v1.0
+SHIP_NET_BRIDGE_VERSION = v1.1
 SHIP_NET_BRIDGE_SITE = https://github.com/MooseTheBrown/ship-net-bridge
 SHIP_NET_BRIDGE_SITE_METHOD = git
 SHIP_NET_BRIDGE_GOMOD=github.com/moosethebrown/ship-net-bridge
