732ccf4317d3b54918a436458335f631f38971e1
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2024-05-09 11:00:59 +0300

    Port for buildroot 2024.02.1 and raspberry pi 4

---
+++ package/ship-net-bridge/ship-net-bridge.mk
@@ -0,0 +1,6 @@
+SHIP_NET_BRIDGE_VERSION = v1.1
+SHIP_NET_BRIDGE_SITE = https://github.com/MooseTheBrown/ship-net-bridge
+SHIP_NET_BRIDGE_SITE_METHOD = git
+SHIP_NET_BRIDGE_GOMOD=github.com/moosethebrown/ship-net-bridge
+
+$(eval $(golang-package))
