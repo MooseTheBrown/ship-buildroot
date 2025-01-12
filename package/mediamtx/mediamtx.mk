da14ee3213d8a8960afdf225eb055635f15c7864
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-01-12 19:08:23 +0300

    Build mediamtx without mediamtx-rpicamera
    Due to difficulties with linking mediamtx-rpicamera with custom libcamera
    version, mediamtx will be used as an RTSP server, and video stream from
    rpi camera will be published to it using gstreamer. Hence mediamtx-
    rpicamera is not needed, rpi camera won't be used by mediamtx directly.

--- package/mediamtx/mediamtx.mk
+++ package/mediamtx/mediamtx.mk
@@ -1,14 +1,12 @@
 MEDIAMTX_SITE = https://github.com/bluenviron/mediamtx
 MEDIAMTX_VERSION = v1.11.0
 MEDIAMTX_SITE_METHOD = git
-MEDIAMTX_DEPENDENCIES = mediamtx-rpicamera
 
 MEDIAMTX_INSTALL_STAGING = YES
 MEDIAMTX_INSTALL_TARGET = YES
 
 define MEDIAMTX_SETUP
-	go generate ./...
-	cp -f $(TARGET_DIR)/usr/bin/mtxrpicam $(MEDIAMTX_SRC_DIR)/internal/staticsources/rpicamera/
+	cd $(BUILD_DIR)/mediamtx-v1.11.0 && $(HOST_DIR)/bin/go generate ./...
 endef
 
 MEDIAMTX_PRE_CONFIGURE_HOOKS += MEDIAMTX_SETUP
