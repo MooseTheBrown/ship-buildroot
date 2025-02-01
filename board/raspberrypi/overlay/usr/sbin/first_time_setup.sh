d34905c50ba3837557786e906007a3ea387126b8
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-02-01 17:35:33 +0300

    Add ship-position service

--- board/raspberrypi/overlay/usr/sbin/first_time_setup.sh
+++ board/raspberrypi/overlay/usr/sbin/first_time_setup.sh
@@ -5,6 +5,7 @@ systemctl enable rfkill_unblock.timer
 systemctl enable wpa_supplicant.timer
 systemctl enable udhcpc.service
 systemctl enable ship-control.service
+systemctl enable ship-position.service
 systemctl enable ship-net-bridge.service
 systemctl enable mediamtx.service
 systemctl enable rpicam-vid.service
