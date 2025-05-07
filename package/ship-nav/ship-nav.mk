dc60e2c5b613442ef2a87c59dee0705e028bb057
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-05-07 18:37:57 +0300

    Add ship-nav and up ship-net-bridge to 2.0

---
+++ package/ship-nav/ship-nav.mk
@@ -0,0 +1,6 @@
+SHIP_NAV_VERSION = v1.0
+SHIP_NAV_SITE = https://github.com/MooseTheBrown/ship-nav
+SHIP_NAV_SITE_METHOD = git
+SHIP_NAV_GOMOD=github.com/moosethebrown/ship-nav
+
+$(eval $(golang-package))
