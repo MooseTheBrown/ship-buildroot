ee72b9757246c61fbbe5bb4cf7da8e594b853408
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-05-24 17:34:54 +0300

    Stream rpicam video to moosethebrown.ru

--- board/raspberrypi/overlay/usr/bin/rpicam-vid.sh
+++ board/raspberrypi/overlay/usr/bin/rpicam-vid.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/rpicam-vid -t 0 -n --inline -o - | /usr/bin/gst-launch-1.0 fdsrc fd=0 ! h264parse ! rtspclientsink protocols=tcp location=rtsp://localhost:8554/cam
+/usr/bin/rpicam-vid -t 0 -n --inline -o - | /usr/bin/gst-launch-1.0 fdsrc fd=0 ! h264parse ! rtspclientsink protocols=tcp location=rtsp://moosethebrown.ru:8554/cam
