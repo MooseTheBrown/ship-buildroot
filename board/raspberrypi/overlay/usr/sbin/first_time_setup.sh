2bd91b0d8a54fbd58f56761cc8491d568c238628
Mikhail Sapozhnikov <masapozhnikov@yandex.ru>
2025-10-19 18:18:55 +0300

    Adjust default ship configuration and increase image size to 50G

--- board/raspberrypi/overlay/usr/sbin/first_time_setup.sh
+++ board/raspberrypi/overlay/usr/sbin/first_time_setup.sh
@@ -7,7 +7,5 @@ systemctl enable udhcpc.service
 systemctl enable ship-control.service
 systemctl enable ship-position.service
 systemctl enable ship-net-bridge.service
-systemctl enable mediamtx.service
 systemctl enable rpicam-vid.service
 chmod 0600 /root/.ssh/id_rsa
-systemctl enable sshtun.service
